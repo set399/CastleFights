ef9c6a7999db2a02667ba224873cbedd7518f631
set399 <melswep8080petties@gmail.com>
2026-07-01 23:13:47 +0200

    Added empty inventory message

--- index.js
+++ index.js
@@ -1414,6 +1414,7 @@ You have opened a ${lootboxEmojis[box]} **${lootboxNames[box]}** and received:
                 let inv = '';
                 Object.keys(db[user].inventory).reverse().forEach(skin => {
                     inv += inventoryDisplay(skin, db[user].inventory[skin]);
+                    if (inv == '') inv = 'This user does not own any skins.';
                 });
                 return message.reply({
                     embeds: [
