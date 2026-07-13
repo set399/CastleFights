9048b455950182a97adb70c65811cb08ba014778
set399 <melswep8080petties@gmail.com>
2026-07-13 22:07:45 +0200

    Optimized CFMap class constructor to have already included clear() function that executes anyway

--- cf/classes.js
+++ cf/classes.js
@@ -16,14 +16,8 @@ const textures = {
 };
 class CFMap {
     constructor(title) {
-        this.data = [];
         this.title = title;
-        for (var x = 0; x < 20; x++) {
-            this.data.push([]);
-            for (var y = 0; y < 9; y++) {
-                this.data[x].push({id: 'EMPTY', health: 0});
-            }
-        };
+        this.clear();
     }
     set(x, y, id, hp, props = {}) {
         x = Math.floor(x);
