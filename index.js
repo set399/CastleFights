106735919ead8f2e31ad21721fdecc7e0e7a3208
set399 <melswep8080petties@gmail.com>
2026-08-10 17:14:13 +0200

    Changed title in request button modal to not show category

--- index.js
+++ index.js
@@ -2481,7 +2481,7 @@ cl.on('interactionCreate', async interaction => {
             if (db[user].requestTs > Date.now()) return await interaction.reply({content: `:no_entry_sign: **You have already have made a request in the past 24 hours, you can create a new one: <t:${db[user].requestTs}:R>**`, flags: 64});
             const modal = new ModalBuilder()
                 .setCustomId('requestModal_' + category + '_' + user)
-                .setTitle(`Submit Request (Category: ${requestCategory[category]})`);
+                .setTitle(`Submit a Castle Fights Request`);
             const requestInput = new TextInputBuilder()
                 .setCustomId('requestText_' + user)
                 .setLabel(`Enter the text for your ${category} request`)
